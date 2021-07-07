263d787dc1bdf0c84c0ff83f5f1801f9fd89a827
Seo-Hongju <tjghdwn0824@naver.com>
2021-07-07 19:00:21 +0900

    part 6-4 commit

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -8,25 +8,28 @@ class ValidationSample extends Component{
         validated: false
     }
 
-    hadleChange = (e) => {
+    handleChange = (e) => {
         this.setState({
             password: e.target.value
         });
     }
 
-    hadleButtonClick = () => {
+    handleButtonClick = () => {
         this.setState({
             clicked: true,
             validated: this.state.password === '0000'
-        })
+        });
+        this.input.focus();
     }
 
     render() {
         return (
             <div>
                 <input
+                    ref={(ref) => this.input=ref}
                     type="password"
-                    value={this.hadleChange}
+                    value={this.state.password}
+                    onChange={this.handleChange}
                     className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''} 
                 />
                 <button onClick={this.handleButtonClick}>검증하기</button>
